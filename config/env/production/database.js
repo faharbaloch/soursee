c33ae960117e77f85517ad0c85926eb3a9df1d38
sfbdigitalservices <sfbdigitalservices@gmail.com>
2024-11-26 06:20:35 +0500

    Your commit message

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -15,17 +15,21 @@
     //   },
     // });
 
+    // strapi-api/config/database.js
 module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
       connection: {
-        host: env('PGHOST', '127.0.0.1'),
-        port: env.int('PGPORT', 5432),
-        database: env('PGDATABASE', 'strapi'),
-        user: env('PGUSER', 'strapi'),
-        password: env('PGPASSWORD', 'strapi'),
-       ssl: env.bool(true),
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', '0000'),
+          // schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
       },
-      pool: {min: 0}
-    },
-  });
+    });
